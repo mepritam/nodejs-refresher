600b680993e4cd9d2ee66bdf3861c2f1fc37ee2e
pritamInnoraft <pritam.sadhu@innoraft.com>
2020-06-29 16:24:46 +0530

    Array and Object defined.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const fs = require("fs");
-fs.writeFileSync("hello.txt", "Hello from Node JS by Pritam");
+//basic JS objects
+
+let demoObj = {
+  name: "Pritam",
+  age: 25,
+  graduate: true,
+};
+
+console.log(demoObj.name);
+
+// array examples
+
+let demoArray = ["Kolkata", "Mumbai", "Chennai", "Pune"];
+
+for (let city of demoArray) {
+  console.log(city);
+}
+
+console.log(demoArray.map((cityName) => "City:" + cityName));
+console.log(demoArray);
